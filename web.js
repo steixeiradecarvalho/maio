db0bfa1f85d723e1c695bbb306a5ca0f1fef75f1
Sergio Teixeira <steixeiradecarvalho@gmail.com>
2017-01-28 20:00:24 -0200

    Add files via upload

--- web.js
+++ web.js
@@ -3,10 +3,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:5000
+var port = process.env.PORT || 8000
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(5000);
+app.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 
